8c345b3eaf73ebf53487d85e7e80ba1785772882
jvsphe <arrezajosephs@gmail.com>
2024-05-02 08:01:15 +0400

    Add getter & setter methods

--- src/main/java/com/stir/cscu9t4practical1/SprintEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/SprintEntry.java
@@ -12,6 +12,35 @@ public class SprintEntry extends Entry {
         this.recoveryTime = rt;
     }
 
-    
-    
+    public String getSessionType() {
+        return this.sessionType;
+    }
+
+    public void setSessionType(String sessionType) {
+        this.sessionType = sessionType;
+    }
+
+    public int getRepetitions() {
+        return this.repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public int getRecoveryTime() {
+        return this.recoveryTime;
+    }
+
+    public void setRecoveryTime(int recoveryTime) {
+        this.recoveryTime = recoveryTime;
+    }
+
+    @Override
+    public String getEntry() {
+        String result = getName() + " did " + repetitions + " " + sessionType + " (with " + recoveryTime + " min. recovery between); running " +
+                getDistance() + " km in " + getHour() + ":" + getMin() + ":" + getSec() +
+                " on " + getDay() + "/" + getMonth() + "/" + getYear() + "\n";
+        return result;
+    }
 }
--- src/main/java/com/stir/cscu9t4practical1/SwimEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/SwimEntry.java
@@ -2,15 +2,25 @@ package com.stir.cscu9t4practical1;
 
 public class SwimEntry extends Entry {
     private String location;
-    private int repetitions;
-    private int recoveryTime;
 
-    public SwimEntry(String n, int d, int m, int y, int h, int min, int s, float dist, String loc, int rep, int rt) {
+    public SwimEntry(String n, int d, int m, int y, int h, int min, int s, float dist, String loc) {
         super(n, d, m, y, h, min, s, dist);
         this.location = loc;
-        this.repetitions = rep;
-        this.recoveryTime = rt;
     }
 
-    
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String getEntry() {
+        String result = super.getName() + " swam " + getDistance() + " km " + (location.equals("pool") ? "in the pool" : "outdoors") +
+                        " in " + getHour() + ":" + getMin() + ":" + getSec() +
+                        " on " + getDay() + "/" + getMonth() + "/" + getYear() + "\n";
+        return result;
+    }
 }
